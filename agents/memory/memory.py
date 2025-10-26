541b719bc1d83219ecf80f5932f581e3514974fb
BiscuitsMaker <138965807+BiscuitsMaker@users.noreply.github.com>
2025-10-26 14:48:15 +0800

    Add files via upload

---
+++ agents/memory/memory.py
@@ -0,0 +1,23 @@
+from langchain.memory import ConversationBufferWindowMemory, ConversationBufferMemory
+import config
+
+def create_memory(memory_type: str = "buffer_window"):
+    if memory_type == "buffer_window":
+        memory = ConversationBufferWindowMemory(
+            k=config.MEMORY_K,               
+            memory_key="chat_history",
+            return_messages=True  
+        )
+    elif memory_type == "buffer":
+        memory = ConversationBufferMemory(
+            memory_key="chat_history",
+            return_messages=True 
+        )
+    else:
+        memory = ConversationBufferMemory(
+            memory_key="chat_history",
+            return_messages=True 
+        )
+    
+    return memory
+
